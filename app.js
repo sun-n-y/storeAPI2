f564e0ea46a774ffa3e5a9562744bda3a63902eb
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-01 16:15:17 -0500

    add basic express server

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('04 Store API')
+require('dotenv').config();
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const app = express();
+
+app.use(express.json());
+
+//route
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+//middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening to port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
