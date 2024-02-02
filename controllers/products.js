15370efe8f5c4afd7dfe109554b7d0acb667a8c2
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-02 16:20:19 -0500

    add pagination filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({}).limit(1).skip(1);
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
@@ -37,6 +37,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
 
