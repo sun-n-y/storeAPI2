261c9c6568cf9f37e9978662c79935365d906d23
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-02 16:05:55 -0500

    add select filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ut';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
 
@@ -30,11 +27,15 @@ const getAllProducts = async (req, res) => {
   let result = Product.find(queryObject);
 
   if (sort) {
-    console.log(sort);
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('-createdAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
 
   const products = await result;
