99c3c632cc9b2cf8cadf54e6cab0b0b075f5e445
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-02 14:58:15 -0500

    add name regex filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,33 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: 'accent chair' });
+  const search = 'ut';
+  const products = await Product.find({
+    name: { $regex: search, $options: 'i' },
+  });
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
-  const products = await Product.find({ featured });
+  const { featured, company, name } = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
