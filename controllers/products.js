de51dbe82f1f6ff0a20f9c62bf022a63512f5f0f
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-02 15:32:06 -0500

    add sort filter

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   const queryObject = {};
 
@@ -27,7 +27,18 @@ const getAllProducts = async (req, res) => {
 
   console.log(queryObject);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('-createdAt');
+  }
+
+  const products = await result;
+
   res.status(200).json({ nbHits: products.length, data: products });
 };
 
